2419c18e70cbff19709c6761aeccbd7b6f91c767
Bianca <biannetta2@huskers.unl.edu>
2023-08-24 12:49:47 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Bianca Iannetta
+ * 2023-08-24
+ *
+ * a program to print my name
+ *
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Bianca Iannetta\n");
+
+  return 0;
+}
