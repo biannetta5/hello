e312d374b104f081b29e7f70e1e692d96f0aa2ee
Samuel-Oleson <Soleson3@huskers.unl.edugit config --global user.email youremail@huskers.unl.edusoleson3@huskers.unl.edugit config -- global user.name Samuel>
2023-08-25 16:23:57 -0500

    Added my major and name to the output

--- hello.c
+++ hello.c
@@ -14,5 +14,8 @@ int main(int argc, char **argv) {
   printf("My name is Bianca Iannetta!\n");
   printf("I am a computer engineering major.\n");
 
+  printf("My name is Samuel Oleson\n");
+  printf("I am a computer engnieering major\n");
+
   return 0;
 }
